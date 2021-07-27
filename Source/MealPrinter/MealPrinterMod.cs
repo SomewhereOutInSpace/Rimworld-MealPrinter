fe6b7258a477615d65ca115a21ba7070ae4794e7
James-DESKTOP <jameshaywood823@gmail.com>
2021-07-26 20:24:07 -0400

    Added printer sound sfx toggle

--- Source/MealPrinter/MealPrinterMod.cs
+++ Source/MealPrinter/MealPrinterMod.cs
@@ -6,8 +6,35 @@ using System.Reflection;
 
 namespace MealPrinter
 {
+    public class MealPrinterSettings : ModSettings 
+    {
+        public bool printSoundEnabled = true;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref printSoundEnabled, "printSoundEnabled", true);
+            base.ExposeData();
+        }
+    }
+    
     public class MealPrinterMod : Mod
     {
+
+        MealPrinterSettings settings;
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.CheckboxLabeled("PrintSFXLabel".Translate(), ref settings.printSoundEnabled);
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "MealPrinterModName".Translate();
+        }
 
         public static bool allowForbidden;
         public static bool allowDispenserFull;
@@ -18,6 +45,7 @@ namespace MealPrinter
 
         public MealPrinterMod(ModContentPack content) : base(content)
         {
+            this.settings = GetSettings<MealPrinterSettings>();
             Log.Message("[MealPrinter] Okay, showtime!");          
             Harmony har = new Harmony("MealPrinter");
             har.PatchAll(Assembly.GetExecutingAssembly());
